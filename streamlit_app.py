04f7615f58617d565db09235a2c7bc90f106c3c1
s0rasky <152986219+s0rasky@users.noreply.github.com>
2023-12-12 06:58:54 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,34 @@
-# Streamlitライブラリをインポート
+申し訳ございませんが、検索結果から適切な情報を得ることができませんでした。名前のバリエーションを１５種類以上提供するためには、男の子と女の子それぞれの名前リストを拡張する必要があります。以下は、男の子と女の子の名前をそれぞれ１５種類以上に増やしたコードの例です。
+
+```python
+# 必要なライブラリのインポート
 import streamlit as st
+import random
 
-# ページ設定（タブに表示されるタイトル、表示幅）
-st.set_page_config(page_title="タイトル", layout="wide")
+# タイトルの表示
+st.title('子供の名前の決定')
 
-# タイトルを設定
-st.title('Streamlitのサンプルアプリ')
+# ユーザーに性別を選択させる
+gender = st.radio("性別を選択してください", ('男の子', '女の子'))
 
-# テキスト入力ボックスを作成し、ユーザーからの入力を受け取る
-user_input = st.text_input('あなたの名前を入力してください')
+# 男の子の名前のリスト
+boy_names = ['太郎', '健太', '隼人', '大輝', '悠斗', '拓海', '樹', '悠太', '慎太郎', '颯太', '翔太', '悠翔', '悠真', '陽斗', '大和']
 
-# ボタンを作成し、クリックされたらメッセージを表示
-if st.button('挨拶する'):
-    if user_input:  # 名前が入力されているかチェック
-        st.success(f'🌟 こんにちは、{user_input}さん! 🌟')  # メッセージをハイライト
-    else:
-        st.error('名前を入力してください。')  # エラーメッセージを表示
+# 女の子の名前のリスト
+girl_names = ['花子', '美咲', 'さくら', '結衣', '莉子', '美羽', '優花', '莉愛', '陽菜', '凛', '心愛', '愛菜', '美音', '瞳', '葵']
 
-# スライダーを作成し、値を選択
-number = st.slider('好きな数字（10進数）を選んでください', 0, 100)
+# 性別に応じて名前を出力
+if gender == '男の子':
+    st.write('あなたのお子様の名前は「' + random.choice(boy_names) + '」になります。')
+else:
+    st.write('あなたのお子様の名前は「' + random.choice(girl_names) + '」になります。')
+```
 
-# 補足メッセージ
-st.caption("十字キー（左右）でも調整できます。")
+このコードでは、男の子と女の子の名前リストを１５種類以上に拡張しています。新しい名前を追加することで、より多様な名前のバリエーションを提供することができます。
 
-# 選択した数字を表示
-st.write(f'あなたが選んだ数字は「{number}」です。')
-
-# 選択した数値を2進数に変換
-binary_representation = bin(number)[2:]  # 'bin'関数で2進数に変換し、先頭の'0b'を取り除く
-st.info(f'🔢 10進数の「{number}」を2進数で表現すると「{binary_representation}」になります。 🔢')  # 2進数の表示をハイライト
+Citations:
+[1] http://minna-atsumare.com/indexbb33.html?id=47
+[2] https://zenn.dev/t_kakei/scraps/92264f6f8a9ad3
+[3] https://voom-creators.line.me/ja/newsroom/
+[4] http://shimadu.net/orosikakaku/sanby025n.pdf
+[5] https://unrepartable25.rssing.com/chan-13723167/all_p1.html
