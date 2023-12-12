714a3576c5cd4eda61c5363b4ba799b8c5008390
s0rasky <152986219+s0rasky@users.noreply.github.com>
2023-12-12 07:04:25 +0000

    l

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,13 +19,3 @@ if gender == '男の子':
     st.write('あなたのお子様の名前は「' + random.choice(boy_names) + '」になります。')
 else:
     st.write('あなたのお子様の名前は「' + random.choice(girl_names) + '」になります。')
-```
-
-このコードでは、男の子と女の子の名前リストを１５種類以上に拡張しています。新しい名前を追加することで、より多様な名前のバリエーションを提供することができます。
-
-Citations:
-[1] http://minna-atsumare.com/indexbb33.html?id=47
-[2] https://zenn.dev/t_kakei/scraps/92264f6f8a9ad3
-[3] https://voom-creators.line.me/ja/newsroom/
-[4] http://shimadu.net/orosikakaku/sanby025n.pdf
-[5] https://unrepartable25.rssing.com/chan-13723167/all_p1.html
